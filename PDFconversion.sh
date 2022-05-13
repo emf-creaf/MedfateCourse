f771e954ff863712ae8e8370cc03800cad7d35c4
Miquel De Cáceres <miquelcaceres@gmail.com>
2022-05-13 21:27:29 +0200

    learning css

---
+++ PDFconversion.sh
@@ -0,0 +1,4 @@
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '1_2_IntroductionToMedfate.html' '1_2_IntroductionToMedfate.pdf'
+`npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '1_3_ModelInputs.html' '1_3_ModelInputs.pdf'
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '1_4_ModelInputs_exercise.html' '1_4_ModelInputs_exercise.pdf'
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '2_2_ForestWaterEnergyBalance_practice.html' '2_2_ForestWaterEnergyBalance_practice.pdf'
