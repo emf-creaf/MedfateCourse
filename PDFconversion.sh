337777dd4c2e132cd4af7cbed61028fdd0fcb9a1
Miquel De Cáceres <miquelcaceres@gmail.com>
2022-05-17 18:44:29 +0200

    progressing with exercice #3

--- PDFconversion.sh
+++ PDFconversion.sh
@@ -2,6 +2,6 @@
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '1_3_ModelInputs.html' 'pdf/1_3_ModelInputs.pdf'
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '1_4_ModelInputs_exercise.html' 'pdf/1_4_ModelInputs_exercise.pdf'
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '2_2_ForestWaterEnergyBalance_practice.html' 'pdf/2_2_ForestWaterEnergyBalance_practice.pdf'
-`npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '2_3_ForestWaterEnergyBalance_exercise.html' 'pdf/2_3_ForestWaterEnergyBalance_exercise.pdf'
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '2_3_ForestWaterEnergyBalance_exercise.html' 'pdf/2_3_ForestWaterEnergyBalance_exercise.pdf'
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '3_2_ForestGrowthDynamics_practice.html' 'pdf/3_2_ForestGrowthDynamics_practice.pdf'
-# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '3_3_ForestGrowthDynamics_exercise.html' 'pdf/3_3_ForestGrowthDynamics_exercise.pdf'
+`npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '3_3_ForestGrowthDynamics_exercise.html' 'pdf/3_3_ForestGrowthDynamics_exercise.pdf'
