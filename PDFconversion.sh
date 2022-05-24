8d22da834d6e22d9f8ffef9fc788df081efe446b
Miquel De Cáceres <miquelcaceres@gmail.com>
2022-05-24 20:13:36 +0200

    4.1 progress

--- PDFconversion.sh
+++ PDFconversion.sh
@@ -5,5 +5,5 @@
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '2_3_ForestWaterEnergyBalance_exercise.html' 'pdf/2_3_ForestWaterEnergyBalance_exercise.pdf'
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '3_2_ForestGrowthDynamics_practice.html' 'pdf/3_2_ForestGrowthDynamics_practice.pdf'
 # `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '3_3_ForestGrowthDynamics_exercise.html' 'pdf/3_3_ForestGrowthDynamics_exercise.pdf'
-`npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '4_1_LargeScaleSimulations_practice.html' 'pdf/4_1_LargeScaleSimulations_practice.pdf'
-`npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '4_2_ClosureFeedback.html' 'pdf/4_2_ClosureFeedback.pdf'
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '4_1_LargeScaleSimulations_practice.html' 'pdf/4_1_LargeScaleSimulations_practice.pdf'
+# `npm bin`/decktape '--chrome-arg=--allow-file-access-from-files' '4_2_ClosureFeedback.html' 'pdf/4_2_ClosureFeedback.pdf'
